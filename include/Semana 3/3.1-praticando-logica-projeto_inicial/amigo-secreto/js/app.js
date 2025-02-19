ab031f9de49f38652749f440dea085e8626493c0
sans-script <alexhere.user@gmail.com>
2025-02-19 16:59:14 -0300

    feat: enhance secret friend drawing functionality with dynamic list and pairing

--- include/Semana 3/3.1-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ include/Semana 3/3.1-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -1,21 +1,34 @@
+let amigos = [];
+
 function adicionar() {
   let amigo = document.getElementById("nome-amigo");
   let lista = document.getElementById("lista-amigos");
+  amigos.push(amigo.value);
 
   if (lista.textContent == "") {
     lista.textContent = amigo.value;
   } else {
     lista.textContent += ", " + amigo.value;
   }
+  amigo.value = "";
 }
 
 function sortear() {
-  embaralhar(listaAmigos);
+  embaralha(amigos);
+  let sorteio = document.getElementById("lista-sorteio");
+
+  for (let i = 0; i < amigos.length; i++) {
+    if (i == amigos.length - 1) {
+      sorteio.innerHTML += amigos[i] + " --> " + amigos[0] + "<br>";
+    } else {
+      sorteio.innerHTML += amigos[i] + " --> " + amigos[i + 1] + "<br>";
+    }
+  }
 }
 
-function embaralhar(lista) {
-  for (let indice = lista.length - 1; indice > 0; indice--) {
-    const indiceAleatorio = Math.floor(Math.random() * (indice + 1));
+function embaralha(lista) {
+  for (let indice = lista.length; indice; indice--) {
+    const indiceAleatorio = Math.floor(Math.random() * indice);
 
     // Atribuição via destrucuring
     [lista[indice - 1], lista[indiceAleatorio]] = [
